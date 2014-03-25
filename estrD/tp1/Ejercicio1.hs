dc91f49c7117487fb234fb31c0fa4600195045ea
Federico Aloi <federico.aloi@gmail.com>
2014-03-25 11:30:01 -0300

    Casi terminado el ejercicio 1 del tp1 de estrD

--- estrD/tp1/Ejercicio1.hs
+++ estrD/tp1/Ejercicio1.hs
@@ -1,13 +1,16 @@
 module Ejercicio1 where
 
+-- 1
 mapSucesor :: [Int] -> [Int]
 mapSucesor [] = []
 mapSucesor (x:xs) = (x + 1) : mapSucesor xs
 
+-- 2
 filterPositivos :: [Int] -> [Int]
 filterPositivos [] = []
 filterPositivos (x:xs) = if x > 0 then x : filterPositivos xs else filterPositivos xs
 
+-- 3
 esPrimo :: Int -> Bool
 esPrimo = undefined
 
@@ -15,31 +18,97 @@ filterPrimos :: [Int] -> [Int]
 filterPrimos [] = []
 filterPrimos (x:xs) = if esPrimo x then x : filterPrimos xs else filterPrimos xs
 
+-- 4
 zipMaximos1 :: [Int] -> [Int] -> [Int]
 zipMaximos1 [] _ = []
 zipMaximos1 (x:xs) (y:ys) = max x y : zipMaximos1 xs ys
 
+-- 5
 zipMaximos2 :: [Int] -> [Int] -> [Int]
 zipMaximos2 [] _ = []
 zipMaximos2 _ [] = []
 zipMaximos2 (x:xs) (y:ys) = max x y : zipMaximos2 xs ys
 
+-- 6
 zipSort :: [Int] -> [Int] -> [(Int, Int)]
 zipSort [] _ = []
 zipSort (x:xs) (y:ys) = (min x y, max x y) : zipSort xs ys
 
+-- 7
 mapLongitudes :: [[a]] -> [Int]
 mapLongitudes [] = []
 mapLongitudes (x:xs) = length x : mapLongitudes xs
 
+-- 8
 longitudMayorA :: [[a]] -> Int -> [[a]]
 longitudMayorA [] _ = []
 longitudMayorA (x:xs) n = if length x > n then x : longitudMayorA xs n else longitudMayorA xs n
 
+-- 9
 mapCuadrados :: [Int] -> [Int]
 mapCuadrados [] = []
 mapCuadrados (x:xs) = if x > 0 then (x * x) : mapCuadrados xs else mapCuadrados xs
 
+-- 10
 sumaPar :: [(Int, Int)] -> [Int]
 sumaPar [] = []
 sumaPar (x:xs) = (fst x + snd x) : sumaPar xs
+
+-- 11
+data Fecha = Fecha Int Int Int deriving (Show, Eq)
+
+data Persona = Persona {
+	nombre :: String,
+	apellido :: String,
+	nacimiento :: Fecha
+} deriving (Show, Eq)
+
+esMenor :: Fecha -> Fecha -> Bool
+esMenor (Fecha dia1 mes1 anio1) (Fecha dia2 mes2 anio2)
+	| anio1 < anio2 = True
+	| anio1 == anio2 && mes1 < mes2 = True
+	| anio1 == anio2 && mes1 == mes2 && dia1 < dia2 = True
+	| otherwise = False
+
+takePersonas :: [Persona] -> Fecha -> [Persona]
+takePersonas [] _ = []
+takePersonas (p:ps) fecha
+	| esMenor (nacimiento p) fecha = p : takePersonas ps fecha
+	| otherwise = []
+
+-- 12
+data Pizza = Pizza {
+	ingredientes :: [String],
+	precio :: Int
+} deriving (Show, Eq)
+
+dropPrecio :: [Pizza] -> [Pizza]
+dropPrecio [] = []
+dropPrecio (p:ps)
+	| precio p <= 30 = dropPrecio ps
+	| otherwise = p : dropPrecio ps
+
+-- 13
+map' :: (a -> b) -> [a] -> [b]
+map' _ []= []
+map' f (x:xs) = f x : map' f xs
+
+takeNombresPersonas :: [Persona] -> Fecha -> [String]
+takeNombresPersonas ps fecha = map' (nombre) (takePersonas ps fecha)
+
+-- 14
+data Color = Rojo | Verde | Azul deriving (Show, Eq)
+type ParColor = (Color, Int)
+
+sumParesColor :: [ParColor] -> [ParColor] -> [ParColor]
+sumParesColor = undefined
+
+-- 15
+reversa :: [Int] -> [Int]
+reversa [] = []
+reversa (x:xs) = reversa xs ++ [x]
+
+-- 16 
+aplanar :: [[Int]] -> [Int]
+aplanar [] = []
+aplanar (x:xs) = x ++ aplanar xs
--- estrD/tp1/Ejercicio1Spec.hs
+++ estrD/tp1/Ejercicio1Spec.hs
@@ -58,3 +58,35 @@ main = hspec $ do
   describe "sumaPar" $ do
     it "dada una lista de pares, devuelve una nueva lista en la que cada elemento es la suma de los elementos de cada par" $
       sumaPar [(2, 1), (3, 7), (4, 4)] `shouldBe` [3, 10, 8]
+
+  describe "esMenor" $ do
+    it "devuelve True si la primera fecha es menor a la segunda" $
+      esMenor (Fecha 11 02 2014) (Fecha 12 02 2014) `shouldBe` True
+
+  describe "takePersonas" $ do
+    describe "dada una lista de personas ordenada por fecha" $ do
+      it "y una fecha mayor al nacimiento de algunas de las personas, devuelve las personas que cumplen anios antes de la fecha dada" $
+        takePersonas [Persona "Juan" "Cruz" (Fecha 30 10 1991), Persona "Pepe" "Lui" (Fecha 1 11 1991), Persona "JJ" "Lopez" (Fecha 3 12 1991)] (Fecha 31 10 1991) `shouldBe` [Persona "Juan" "Cruz" (Fecha 30 10 1991)]
+      
+      it "y una fecha menor al nacimiento de todas las personas, devuelve una lista vacia" $
+        takePersonas [Persona "Juan" "Cruz" (Fecha 30 10 1991), Persona "Pepe" "Lui" (Fecha 1 11 1991)] (Fecha 20 10 1990) `shouldBe` []
+
+  describe "dropPrecio" $ do
+    it "dada una lista de Pizzas en orden ascendente por precio, devuelve aquellas con precio superior a $30" $
+      dropPrecio [(Pizza ["queso", "tomate"] 25), (Pizza ["queso", "tomate", "jamon"] 31), (Pizza ["queso", "tomate", "jamon", "morron"] 40)] `shouldBe` [(Pizza ["queso", "tomate", "jamon"] 31), (Pizza ["queso", "tomate", "jamon", "morron"] 40)]
+
+  describe "takeNombresPersonas" $ do
+    it "dada una lista de Personas y una fecha devuelve los nombres de las personas que nacieron antes de dicha fecha" $
+      takeNombresPersonas [Persona "Juan" "Cruz" (Fecha 30 10 1991), Persona "Pepe" "Lui" (Fecha 1 11 1991)] (Fecha 31 10 1991) `shouldBe` ["Juan"]
+
+  describe "sumParesColor" $ do
+    it "dadas dos listas de ParColor, devuelve una lista de Par-Color donde la cantidad de cada elemento es la suma del elemento de ese color en cada lista" $
+      sumParesColor [(Rojo, 12), (Verde, 1)] [(Azul, 8), (Rojo, 3)] `shouldBe` [(Rojo, 15), (Verde, 1), (Azul, 8)]
+
+  describe "reversa" $ do
+    it "dada una lista de enteros, devuelve la lista con los mismos elementos de atras para adelante" $
+      reversa [1, 2, 3] `shouldBe` [3, 2, 1]
+
+  describe "aplanar" $ do
+    it "dada una lista de listas, devuelve una unica lista con todos sus elementos" $
+      aplanar [[1, 2], [3], [4, 5, 6, 7]] `shouldBe` [1, 2, 3, 4, 5, 6, 7]
