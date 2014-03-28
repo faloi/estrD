186f01e12f70e16cac41cf9d9fdd097b81a351f6
Federico Aloi <federico.aloi@gmail.com>
2014-03-27 23:18:49 -0300

    Terminado el ejercicio 3 del tp1

--- estrD/tp1/Ejercicio3.hs
+++ estrD/tp1/Ejercicio3.hs
@@ -14,5 +14,35 @@ last' (x:xs) = last xs
 
 -- 3
 -- precond: que s este en xs
+dondeEsta :: [String] -> String -> Int -> Int
+dondeEsta (x:xs) s i
+	| x == s = i
+	| otherwise = dondeEsta xs s i+1
+
 lookUp :: [String] -> String -> Int
-lookUp [] _ = undefined
+lookUp xs s = dondeEsta xs s 1
+
+-- 4
+takeN :: [a] -> Int -> [a]
+takeN _ 0 = []
+takeN [] _ = []
+takeN (x:xs) n = x : takeN xs (n-1)
+
+-- 5
+dropN :: [a] -> Int -> [a]
+dropN xs 0 = xs
+dropN [] _ = []
+dropN (x:xs) n = dropN xs (n-1)
+
+-- 6
+-- precond: que la clave este en la lista
+valorParaClave :: Eq a => [(a, b)] -> a -> b
+valorParaClave (p:ps) x
+	| fst p == x = snd p
+	| otherwise = valorParaClave ps x
+
+-- 7
+-- precond: length ns >= 1
+maximo :: [Int] -> Int
+maximo [x] = x
+maximo (x:xs) = max x (maximo xs)
--- estrD/tp1/Ejercicio3Spec.hs
+++ estrD/tp1/Ejercicio3Spec.hs
@@ -17,3 +17,28 @@ main = hspec $ do
   describe "lookUp" $ do 
     it "dada una lista de strings y un string s devuelve la posicion de la lista recibida en que se encuentra s" $
       lookUp ["hola", "que", "onda"] "que" `shouldBe` 2
+
+  describe "takeN" $ do
+    it "dada una lista y un numero n, devuelve los primeros n elementos" $
+      takeN [1, 2, 3, 4] 3 `shouldBe` [1, 2, 3]
+
+    it "dada una lista de longitud menor a n y un numero n, devuelve toda la lista" $
+      takeN [1, 2, 3] 4 `shouldBe` [1, 2, 3]
+
+  describe "dropN" $ do
+    it "dada una lista y un numero n, devuelve una lista sin los primeros n elementos" $
+      dropN [1, 2, 3, 4] 3 `shouldBe` [4]
+
+    it "dada una lista de longitud menor a n y un numero n, devuelve una lista vacia" $
+      dropN [1, 2, 3] 4 `shouldBe` []
+
+  describe "valorParaClave" $ do
+    it "dada una lista de pares clave-valor y una clave, devuelve el valor asociado" $
+      valorParaClave [(1, "uno"), (2, "dos")] 2 `shouldBe` "dos"
+
+  describe "maximo" $ do
+    it "dada una lista de un elemento, devuelve ese elemento" $
+      maximo [2] `shouldBe` 2
+
+    it "dada una lista de 2 o mas numeros, devuelve el mayor" $
+      maximo [4, 6, 1] `shouldBe` 6
