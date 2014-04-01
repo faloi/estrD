ce3cfb195b6afb02f7e6fab54dfa820da58a1a83
Federico Aloi <federico.aloi@gmail.com>
2014-04-01 10:55:56 -0300

    tree2list, levelT y leves

--- estrD/Tree.hs
+++ estrD/Tree.hs
@@ -30,3 +30,18 @@ minT (NodeT x EmptyT EmptyT) = x
 minT (NodeT x t1 EmptyT) = min x (minT t1)
 minT (NodeT x EmptyT t2) = min x (minT t2)
 minT (NodeT x t1 t2) = min x (min (minT t1) (minT t2))
+
+tree2list :: Tree a -> [a]
+tree2list EmptyT = []
+tree2list (NodeT x t1 t2) = x : tree2list t1 ++ tree2list t2
+
+levelT :: Tree a -> Int -> [a]
+levelT _ 0 = []
+levelT EmptyT _ = []
+levelT (NodeT x t1 t2) n
+  | n == 1    = [x] 
+  | otherwise = levelT t1 (n-1) ++ levelT t2 (n-1)
+
+levels :: Tree a -> [[a]]
+levels EmptyT = [[]]
+levels t = map (levelT t) [1..heightT t]
--- estrD/TreeSpec.hs
+++ estrD/TreeSpec.hs
@@ -37,3 +37,57 @@ main = hspec $ do
                 EmptyT
               )
             ) `shouldBe` 0
+
+  describe "tree2list" $ do
+    it "dado un arbol vacio, devuelve una lista vacia" $
+      tree2list EmptyT `shouldBe` ([] :: [Int])
+
+    it "dado un arbol no vacio, devuelve una lista con todos sus elementos" $
+      tree2list (NodeT 2 (NodeT 3 (leaf 4) (leaf 5)) (NodeT 6 (leaf 7) (leaf 8))) `shouldBe` [2, 3, 4, 5, 6, 7, 8]
+
+  describe "levelT" $ do
+    it "dado un arbol vacio y cualquier n, devuelve una lista vacia" $
+      levelT EmptyT 8 `shouldBe` ([] :: [Int])
+
+    it "dado un arbol y 0, devuelve una lista vacia" $
+      levelT EmptyT 0 `shouldBe` ([] :: [Int])
+
+    it "dado un arbol y 1, devuelve una lista con su raiz" $
+      levelT (leaf 5) 1 `shouldBe` [5]
+
+    it "dado un arbol y 2, devuelve una lista los elementos del nivel 2" $
+      levelT (NodeT 1 (leaf 2) (leaf 3)) 2 `shouldBe` [2, 3]
+
+    it "dado un arbol y cualquier n, devuelve una lista los elementos del nivel n" $
+      levelT (NodeT 1 
+              (NodeT 2 
+                (leaf 4) 
+                (leaf 7)
+              ) 
+              (NodeT 1 
+                (NodeT 6
+                  (leaf 0) 
+                  EmptyT
+                )
+                EmptyT
+              )
+            ) 3 `shouldBe` [4, 7, 6]
+
+  describe "levels" $ do
+    it "dado un arbol vacio, devuelve una lista de listas vacia" $
+      levels EmptyT `shouldBe` ([[]] :: [[Int]])
+
+    it "dado un arbol no vacio, devuelve la lista con la lista de cadda nivel" $
+      levels (NodeT 1 
+              (NodeT 2 
+                (leaf 4) 
+                (leaf 7)
+              ) 
+              (NodeT 1 
+                (NodeT 6
+                  (leaf 0) 
+                  EmptyT
+                )
+                EmptyT
+              )
+            ) `shouldBe` [[1], [2, 1], [4, 7, 6], [0]]
